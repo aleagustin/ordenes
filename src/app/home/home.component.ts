09abcb59e487a82641f4fde06a0b5aedc9af3fdb
alvaroTronchoni <alvaroroto@hotmail.com>
2021-03-09 13:15:21 +0100

    version2

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -12,7 +12,7 @@ import { Router } from '@angular/router'
   encapsulation: ViewEncapsulation.None,
 })
 export class HomeComponent implements OnInit {
-  @ViewChild(QrScannerComponent) qrScannerComponent: QrScannerComponent;
+  @ViewChild(QrScannerComponent, {static: false }) qrScannerComponent: QrScannerComponent;
 
   URL: any;
   constructor(private http: HttpClient, private auth: AuthService, private router: Router/*, private qrScannerComponent:QrScannerComponent*/) {}
@@ -20,12 +20,11 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
 
-
   }
 
 
 
-  ngAfterViewInit() {
+  ngAfterViewInit(): void {
 
     this.qrScannerComponent.getMediaDevices().then((devices) => {
       console.log(devices);
@@ -45,30 +44,36 @@ export class HomeComponent implements OnInit {
         }
         if (choosenDev) {
           this.qrScannerComponent.chooseCamera.next(choosenDev);
+          console.log("Detecto mas de una camara")
         } else {
+          /*El Array viene por defecto en la documentación a [0] pero como detecta 2 camaras le asigno el [1]
+            que es la webcam el problema es que deberia entrar en el if choosendev
+          */
           this.qrScannerComponent.chooseCamera.next(videoDevices[0]);
+          console.log("entro detecto una camara")
         }
       }
     });
 
 
    this.qrScannerComponent.capturedQr.subscribe((data) => {
+     console.log("Entro en lectura")
       console.log(data);
 
        /*this.auth.authScan(data).subscribe((res: any) =>{
 
-
-    });*/
+       })*/
+    });
 
     let result = this.auth.authScan();
-
+/*
     if(result) {
 
       this.router.navigate([''], { queryParams: { login: result } })
 
-    }
+    }*/
 
-    })
+    //})
 
 
   }
